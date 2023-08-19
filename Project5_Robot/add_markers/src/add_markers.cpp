f03799512ea9ef6ee83e03f7315dd8d95d36e253
Kishan Billava <Kishanbillava73@gmail.com>
2023-08-19 16:42:02 +0530

    update add_marker package
    update add_marker package

--- Project5_Robot/add_markers/src/add_markers.cpp
+++ Project5_Robot/add_markers/src/add_markers.cpp
@@ -2,7 +2,7 @@
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Pose.h>
-#include <cmath>
+#include <math.h>
 
 double pickupX = -1.0;
 double pickupY = -7.2;
@@ -12,72 +12,92 @@ double dropY = -7.2;
 float x_cor = 0.0;
 float y_cor = 0.0;
 
-bool isPickedUp = false;
-
 void odometryCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+    // Extract the position from the odometry message
+    // geometry_msgs::Pose pose = msg->pose.pose;
     x_cor = msg->pose.pose.position.x;
     y_cor = msg->pose.pose.position.y;
 
-    // Check if robot is close to pickup or drop-off zones
-    double distance_to_pickup = std::hypot(pickupX - x_cor, pickupY - y_cor);
-    double distance_to_dropoff = std::hypot(dropX - x_cor, dropY - y_cor);
+    // Process the position data here
+    // You can use the x, y, and z variables to access the position values
 
-    if (!isPickedUp && distance_to_pickup < 0.5) {
-        ROS_INFO("Robot is close to the pickup zone.");
-        isPickedUp = true;
-    }
-
-    if (isPickedUp && distance_to_dropoff < 0.5) {
-        ROS_INFO("Robot is close to the drop-off zone.");
-    }
+    // ROS_INFO("Robot Position: x_cor = %f, x_cor = %f ", x_cor, x_cor);
 }
 
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "add_markers");
-    ros::NodeHandle nh;
-    ros::Rate r(1); // 1 Hz
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "add_markers");
+  ros::NodeHandle nh;
+  ros::Rate r(1); // 1 Hz
 
-    ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-    ros::Subscriber odomSubscriber = nh.subscribe("/odom", 1000, odometryCallback);
+  ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  // Subscribe to the odometry topic
+  ros::Subscriber odomSubscriber = nh.subscribe("/odom", 10, odometryCallback);
 
-    visualization_msgs::Marker marker;
-    marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time::now();
-    marker.ns = "add_markers";
-    marker.id = 0;
-    marker.type = visualization_msgs::Marker::CUBE;
+  // Create a marker message
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = "map";
+  marker.header.stamp = ros::Time::now();
+  marker.ns = "add_markers";
+  marker.id = 0;
+  marker.type = visualization_msgs::Marker::CUBE;
+  marker.action = visualization_msgs::Marker::ADD;
 
+  ROS_INFO("add_markers: active");
+
+  marker.pose.position.x = 0.0;
+  marker.pose.position.y = 0.0;
+  marker.pose.orientation.w = 1.0;
+  marker.scale.x = 0.5;
+  marker.scale.y = 0.5;
+  marker.scale.z = 0.5;
+  marker.color.r = 0.0;
+  marker.color.g = 1.0;
+  marker.color.b = 0.0;
+  marker.color.a = 1.0;
+  marker.lifetime = ros::Duration();
+
+  // Wait for 5 seconds
+  ros::Duration(1.0).sleep();
+
+
+    // Publish the marker initially at the pickup zone
     marker.pose.position.x = pickupX;
     marker.pose.position.y = pickupY;
-    marker.pose.orientation.w = 1.0;
-    marker.scale.x = 0.5;
-    marker.scale.y = 0.5;
-    marker.scale.z = 0.5;
-    marker.color.r = 0.0;
-    marker.color.g = 1.0;
-    marker.color.b = 0.0;
-    marker.color.a = 1.0;
-    marker.lifetime = ros::Duration();
-
-    ros::Duration(2.0).sleep();
-
     marker.action = visualization_msgs::Marker::ADD;
     marker_pub.publish(marker);
+    // ROS_INFO("Marker published at the pickup zone");
+    // ROS_INFO("Robot Position: pickupX = %f, pickupY = %f ", pickupX, pickupY);
+    // ROS_INFO("Robot Position: x_cor = %f, x_cor = %f ", x_cor, x_cor);
 
-    ros::Duration(1.0).sleep();
+  
+  // Wait for 5 seconds
+  ros::Duration(2.0).sleep();
 
-    while (ros::ok()) {
-        if (isPickedUp) {
-            marker.action = visualization_msgs::Marker::DELETE;
-            marker_pub.publish(marker);
-        } else {
-            marker.action = visualization_msgs::Marker::ADD;
-            marker_pub.publish(marker);
-        }
+  if ( (abs(pickupX - x_cor)<0.5) && (abs(pickupY - y_cor )<0.5 ))
+  {
+  // Hide the marker
+  marker.action = visualization_msgs::Marker::DELETE;
+  marker_pub.publish(marker);
+  ROS_INFO("Marker hidden");
+  }
+  // Wait for 5 seconds
+  ros::Duration(5.0).sleep();
 
-        ros::spinOnce();
-        r.sleep();
-    }
+  if ( (abs(dropX - x_cor)<0.5) && (abs( dropY - y_cor)<0.5) )
+  {
+    // Publish the marker at the drop-off zone
+    marker.pose.position.x = dropX;
+    marker.pose.position.y = dropY;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker_pub.publish(marker);
+    ROS_INFO("Marker published at the drop-off zone");
+  }
+  
+  // Wait for 5 seconds
+  ros::Duration(5.0).sleep();
 
-    return 0;
+  ros::spin();
+
+  return 0;
 }
